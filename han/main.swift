114b52a568684e33b1376802df1422895a19435b
s20171106577 <s20171106577@123.local>
2018-09-29 08:21:21 +0800

    Initial Commit

---
+++ han/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  han
+//
+//  Created by s20171106577 on 2018/9/29.
+//  Copyright © 2018年 s20171106577. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
